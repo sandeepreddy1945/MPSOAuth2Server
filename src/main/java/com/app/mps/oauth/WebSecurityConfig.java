005f9312e8c0e3fb415ef13c64bc6e61442c9162
sandeepreddy1945 <b.sandeepreddy209@gmail.com>
2018-06-20 00:35:48 +0530

    Signed-off-by: sandeepreddy1945 <b.sandeepreddy209@gmail.com>

--- src/main/java/com/app/mps/oauth/WebSecurityConfig.java
+++ src/main/java/com/app/mps/oauth/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/console/**");
+		web.ignoring().antMatchers("/console/**").antMatchers("/swagger/**");
 	}
 
 }
